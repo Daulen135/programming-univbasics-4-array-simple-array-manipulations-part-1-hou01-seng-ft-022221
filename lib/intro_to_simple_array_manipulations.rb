9a0d4c044b83cce2864429bcb1bdb43e81f6f484
Daulen Zhangabylov <daulenz@gmail.com>
2021-01-31 16:28:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,6 +30,25 @@ using_pop(["Africa", "Asia", "Europe", "North America", "South America", "Oceani
  using_shift(["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"])
 
 
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
+shift_with_args
+  
 
+describe "shift_with_args" do 
+  before(:each) do 
+    @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+    @brands_removed = shift_with_args(@ice_cream_brands)
+  end
+  
+  it "takes in an argument of an array and uses the shift method with an argument of 2 to remove and return the first 2 items from the array" do 
+    expect(@brands_removed).to eq(["Blue Bell Creameries", "Ben & Jerry's"])
+  end
+
+  it "decreases the length of the array by 2" do 
+    expect(@brands_removed.size).to eq(2)
+  end
+end
  
   
