e2beda6d1c62f3d089fbaa00ef1cbc5be4628622
Daulen Zhangabylov <daulenz@gmail.com>
2021-01-31 15:22:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,6 +8,6 @@ end
 def using_push(colors_in_the_rainbow,next_color)
   
  colors_in_the_rainbow.push(next_color)
- updated_array=colors_in_the_rainbow
+
 end
 using_push(["red","orange","yellow","green","blue","indigo"],"violet")
